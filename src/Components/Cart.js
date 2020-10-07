9d7ad270c32de8cf1a9293b1057da77e08677be2
gaelle <gaelle.fougere29@gmail.com>
2020-10-07 13:03:17 +0200

    feat: add cart view

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import BookItem from './BookItem'
+import './Cart.css'
 
+const Cart = ({ bookCart, dispatch}) => {
+    return (
+    <div className='cart-container'> 
+        <h2>Votre panier</h2>
+        {
+            bookCart.length > 0 ?
+            bookCart.map((book)=> <BookItem key={book.isbn} book={book} />):
+            'Aucun livre dans le panier'
+        }
 
-const Cart = () => <div> YOUR CART </div>
+    </div>
+    )
+}
 
-export default Cart
+const mapStateToProps=(state)=>{
+    return {bookCart: state.bookCart || []}
+  }
+export default connect(mapStateToProps)(Cart)
