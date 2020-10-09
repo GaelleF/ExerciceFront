1ad2faef9641a126d8c6f8b754c7bd32ca48a051
gaelle <gaelle.fougere29@gmail.com>
2020-10-09 12:27:27 +0200

    feat: add cypress

---
+++ cypress/integration/book_list.spec.js
@@ -0,0 +1,9 @@
+// / <reference types="cypress" />
+before(() => {
+  });
+  describe('book list page', () => {
+    it('should create a new user', () => {
+        cy.visit('http://localhost:3000/');
+    })
+
+  })
