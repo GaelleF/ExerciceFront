5c07394705da85a6d33a88a08077eca56d8b5b4c
gaelle <gaelle.fougere29@gmail.com>
2020-10-09 15:33:03 +0200

    Updates

---
+++ precache-manifest.29029a38ca174128cdcb1d4298a578bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e5b5dd0b24dd160a787e085b3c2160",
+    "url": "/exercice-front/index.html"
+  },
+  {
+    "revision": "9f8f3650e2565e3c26ba",
+    "url": "/exercice-front/static/css/main.80a62c0c.chunk.css"
+  },
+  {
+    "revision": "3fc7160458a227880f63",
+    "url": "/exercice-front/static/js/2.c2289af7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/exercice-front/static/js/2.c2289af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8f3650e2565e3c26ba",
+    "url": "/exercice-front/static/js/main.861fa36e.chunk.js"
+  },
+  {
+    "revision": "61b0fcfd1bffaef5bc4d",
+    "url": "/exercice-front/static/js/runtime-main.6a9b72de.js"
+  },
+  {
+    "revision": "0454ff82a0b50e53ddef85faedd5a856",
+    "url": "/exercice-front/static/media/WizardsMagic.0454ff82.ttf"
+  }
+]);
--- src/Store/configureStore.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux'
-import toggleCart from './Reducers/cartReducer'
-
-
-
-export default createStore(toggleCart)
